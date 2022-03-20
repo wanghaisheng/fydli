99b452842e2cbe38ad0a2c38b11c9286593d1418
Coel May <82788995+coelmay@users.noreply.github.com>
2022-03-21 09:09:57 +1100

    Update new-uuid.js
    Added file id, SPDX identifier.

--- netlify/functions/new-uuid.js
+++ netlify/functions/new-uuid.js
@@ -1,6 +1,13 @@
+//
+// new-uuid.js
+// - Create UUID (primarily for Safari < 15.4)
+//
+// SPDX-License-Identifier: Jam
+//
+
 import { randomUUID } from 'crypto';
 
-exports.handler = async (event, context) => {
+exports.handler = async () => {
 
 	return {
     statusCode: 200,
